36d0ac39c3b792ce98ac1ec91d89eab7b65303b2
code-akimbo <leightonaskew99@gmail.com>
2025-07-16 18:09:23 +0100

    player movement complete

--- player.py
+++ player.py
@@ -25,6 +25,11 @@ class Player(circleshape.CircleShape):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
     
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
+    
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -32,3 +37,7 @@ class Player(circleshape.CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
